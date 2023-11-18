f040b3e92da0ff7aefa9f29b2230b84322309180
Thomas Butler <trbiv@proton.me>
2023-11-17 21:56:01 -0600

    removed .next from remote

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "33f74602caa4fada"; }
+/******/ 		__webpack_require__.h = function() { return "136538393d60a006"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
